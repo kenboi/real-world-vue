ebaaf86b7b561faf32665a38cb54a23450a8455d
kenboi <kendrick.kilat01@gmail.com>
2021-04-09 16:13:58 +0800

    Initial Commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow Town',
+          date: 'January 28 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow Town',
+          date: 'January 28 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow Town',
+          date: 'January 28 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scope>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
