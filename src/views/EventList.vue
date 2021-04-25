b0ea851501d7b5079033bd24e899a9ef76403cba
kenboi <kendrick.kilat01@gmail.com>
2021-04-26 04:28:10 +0800

    Vue Router Lesson 3

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,43 +2,60 @@
   <h1>Events for {{ user.user.name }}</h1>
   <div class="events">
     <EventCard v-for="event in event.events" :key="event.id" :event="event" />
-    <template v-if="page != 1">
-      <router-link :to="{ name: 'EventList', query: { page: page - 1}}" rel = "prev">Prev Page</router-link>
-      <template v-if="hasNextPage"> | </template>
-    </template>
-    <router-link v-if="hasNextPage" :to="{ name: 'EventList', query: { page: page + 1}}"> Next Page </router-link>
+    <div class="pagination">
+      <template v-if="page != 1">
+        <router-link
+          id = "page-prev"
+          :to="{ name: 'EventList', query: { page: page - 1 } }"
+          rel="prev"
+          >&#60; Previous</router-link
+        >
+      </template>
+      <router-link
+      id = "page-next"
+        v-if="hasNextPage"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+      >
+        Next &#62;
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
-import  { mapState } from 'vuex'
+import { mapState } from 'vuex'
+import { watchEffect } from 'vue'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard,
   },
   created() {
-    this.perPage = 3,
-    this.$store.dispatch('event/fetchEvents', {
-      perPage: this.perPage,
-      page: this.page
+    watchEffect(() => {
+      this.events = null
+      this.perPage = 3
+      this.$store.dispatch('event/fetchEvents', {
+        perPage: this.perPage,
+        page: this.page,
+      })
     })
   },
   computed: {
-    page() {
-      console.log(this.$route.query.page)
-      console.log('query page: '+this.$route.query.page)
-      console.log(this.event.events)
-      return parseInt(this.$route.query.page) || 1
-    },
+    // page() {
+    //   console.log(this.$route.query.page)
+    //   console.log('query page: '+this.$route.query.page)
+    //   console.log(this.event.events)
+    //   return parseInt(this.$route.query.page) || 1
+    // },
     hasNextPage() {
       return this.event.eventsTotal > this.page * this.perPage
     },
-    ...mapState(['event','user']),
-    }
+    ...mapState(['event', 'user']),
+  },
 }
 </script>
 
@@ -47,5 +64,21 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
